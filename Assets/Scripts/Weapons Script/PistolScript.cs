85739b420621fc1244ff7236e207147fe3cc0f2a
Allan Bezagrebere <86600046+ChemicalGhost@users.noreply.github.com>
2025-01-12 23:54:19 +0000

    seting up pistol script

--- Assets/Scripts/Weapons Script/PistolScript.cs
+++ Assets/Scripts/Weapons Script/PistolScript.cs
@@ -9,5 +9,71 @@ public class PistolScript : MonoBehaviour
     [SerializeField] int ammoSize = 12;
     [SerializeField] int fireRate = 2;
 
+    LineRenderer lineRenderer;
+
+    [SerializeField] Camera cameraa;
+    void Start()
+    {
+        // Get the Line Renderer component
+        lineRenderer = GetComponent<LineRenderer>();
+        // Ensure the Line Renderer has 2 positions (start and end)
+        lineRenderer.positionCount = 2;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        PointToCrosshair();
+    }
+
+    void PointToCrosshair()
+    {
+        Vector3 rayOriginPos = muzzlePoint.transform.position;
+        Vector3 rayDirection = cameraa.transform.forward;
+        Ray cameraRay = cameraa.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.5f));
+
+        if (Physics.Raycast(cameraRay, out RaycastHit hitInfo, 100.0f))
+
+        {
+            // RenderLine(cameraRay.origin, hitInfo.point);
+
+            // Calculate direction for the second ray
+            Vector3 secondRayDirection = (hitInfo.point - rayOriginPos).normalized;
+
+            // Cast the second ray from the gun towards the first hit point
+            if (Physics.Raycast(rayOriginPos, secondRayDirection, out RaycastHit weaponHitInfo, hitInfo.distance))
+            {
+
+                Debug.Log("Hit SOMETHING" + hitInfo.point + hitInfo.distance);
+                RenderLine(rayOriginPos, weaponHitInfo.point);
+                // Debug.DrawRay(rayOriginPos, secondRayDirection, Color.red);
+
+            }
+            else
+            {
+                Debug.Log("NOTHING");
+                RenderLine(rayOriginPos, weaponHitInfo.point);
+                // Debug.DrawRay(rayOriginPos, secondRayDirection, Color.green);
+
+            }
+
+        }
+        else
+        {
+            // Optional: Extend the line to a maximum distance if no hit
+            Vector3 maxDistancePoint = rayOriginPos + rayDirection * 100.0f;
+            // RenderLine(rayOriginPos, maxDistancePoint);
+            Debug.DrawRay(rayOriginPos, maxDistancePoint, Color.green);
+        }
+    }
+
+    void RenderLine(Vector3 lineOrign, Vector3 lineDestination)
+    {
+        lineRenderer.SetPosition(0, lineOrign);
+        lineRenderer.SetPosition(1, lineDestination);
+        // lineRenderer.sharedMaterial.SetColor("_Color", Color.green);
+    }
+
+
 
 }
